942fd7f89d2086ae106ecbab43af76d04ffd72b2
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 18:39:22 +0500

    Add src\data\products.js

---
+++ src\data\products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+        price: 10.99,
+    image: 'product1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+        price: 9.99,
+    image: 'product2.jpg'
+  },
+  // ...
+];
+
+export default products;
