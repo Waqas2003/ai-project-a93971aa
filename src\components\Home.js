ccb1443b5d927264aeb24f424f491c4f2ff3501b
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 18:39:15 +0500

    Add src\components\Home.js

---
+++ src\components\Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold">Welcome to our ecommerce website!</h1>
+      <p className="text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
